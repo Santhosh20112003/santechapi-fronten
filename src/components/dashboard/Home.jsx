471660d11861a393eeb0b53ca766ead1baff92af
Santhosh <shanmugamsanthosh22@gmail.com>
2024-03-17 01:21:33 +0530

    sjbhdhs

--- src/components/dashboard/Home.jsx
+++ src/components/dashboard/Home.jsx
@@ -80,12 +80,12 @@ function Home() {
             </Link>
           </span>
           <span className="flex items-center justify-center">
-            <Link
-              to="https://santechapitool.vercel.app/"
+            <a target="_blank" rel="noreferrer"
+              href="https://santechapitool.vercel.app/"
               className="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-purple-600 text-white font-semibold py-2 px-5 w-[90%] active:w-full lg:w-[50%] hover:lg:w-[80%] hover:lg:text-gray-800 text-center rounded-full shadow-lg transform transition-all duration-500 ease-in-out hover:lg:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce"
             >
               Try API Tool <i className="fas fa-vial ps-2 text-base"></i>
-            </Link>
+            </a>
           </span>
         </span>
         <span className=" hidden lg:flex">
