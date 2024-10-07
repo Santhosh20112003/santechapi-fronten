f161af45bd6f44d4c566c79faf8d9456288841a1
Santhosh20112003 <shanmugamsanthosh22@gmail.com>
2024-10-07 23:19:18 +0530

    QR code

--- src/components/common/links.jsx
+++ src/components/common/links.jsx
@@ -106,73 +106,73 @@ export const fontendlang = [
     lang: "Vanilla JavaScript",
     img: "https://res.cloudinary.com/hilnmyskv/image/upload/v1724919194/ui-library/icons/javascript.svg",
     code: `async function fetchWeather(location) {<br/>
-      const apiKey = '{YOUR API KEY}'; // Replace with your actual API key<br/>
-      const url = 'https://santechapi.vercel.app/weather/location/'+location;<br/>
+      &nbsp &nbsp const apiKey = '{YOUR API KEY}'; // Replace with your actual API key<br/>
+      &nbsp &nbsp const url = 'https://santechapi.vercel.app/weather/location/'+location;<br/>
       <br/>
-      try {<br/>
-      const response = await fetch(url, {<br/>
-        headers: {<br/>
-          'token': apiKey<br/>
-        }<br/>
-      });<br/>
+      &nbsp &nbsptry {<br/>
+      &nbsp &nbsp &nbsp &nbsp const response = await fetch(url, {<br/>
+      &nbsp &nbsp &nbsp &nbsp  headers: {<br/>
+      &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp    'token': apiKey<br/>
+      &nbsp &nbsp &nbsp &nbsp  }<br/>
+      &nbsp &nbsp &nbsp &nbsp});<br/>
 <br/>
-      if(!response.ok) {<br/>
-    throw new Error('HTTP error! status:' + response.status);<br/>
-  }<br/>
+    &nbsp &nbsp  if(!response.ok) {<br/>
+    &nbsp &nbsp &nbsp &nbsp throw new Error('HTTP error! status:' + response.status);<br/>
+  &nbsp &nbsp }<br/>
 <br/>
-        const weatherData = await response.json();<br/>
-        console.log(weatherData);<br/>
-      } catch (err) {<br/>
-  displayError(err.message);<br/>
-}<br/>
+      &nbsp &nbsp  const weatherData = await response.json();<br/>
+       &nbsp &nbsp console.log(weatherData);<br/>
+      &nbsp &nbsp } catch (err) {<br/>
+&nbsp &nbsp &nbsp &nbsp  displayError(err.message);<br/>
+&nbsp &nbsp }<br/>
     }<br/>`
   },
   {
     lang: "React (JavaScript)",
     img: "https://res.cloudinary.com/hilnmyskv/image/upload/v1724919194/ui-library/icons/react.svg",
-    code: `import React, { useState, useEffect } from 'react'; <br />
+    code: `import React, { useState, useEffect } from 'react'; <br /> <br />
 function App() { <br />
-  const [weatherData, setWeatherData] = useState(null);<br />
-  const [error, setError] = useState(null);<br />
-
-  useEffect(() => {<br />
-    const fetchWeather = async () => {<br />
-      try {<br />
-        const response = await fetch('https://santechapi.vercel.app/weather/location/chennai', {<br />
-          headers: {<br />
-            'token': '{YOUR API KEY}'<br />
-          }<br />
-        });<br />
-
-        if (!response.ok) {<br />
-          throw new Error('HTTP error! status:' + response.status);<br />
-        }<br />
-
-        const data = await response.json();<br />
-        setWeatherData(data);<br />
-      } catch (err) {<br />
-        setError(err.message);<br />
-      }<br />
-    };<br />
+&nbsp &nbsp  const [weatherData, setWeatherData] = useState(null);<br />
+&nbsp &nbsp  const [error, setError] = useState(null);<br />
 <br />
-    fetchWeather();<br />
-  }, []);<br />
+&nbsp &nbsp  useEffect(() => {<br />
+&nbsp &nbsp &nbsp &nbsp    const fetchWeather = async () => {<br />
+&nbsp &nbsp &nbsp &nbsp    try {<br />
+&nbsp &nbsp &nbsp &nbsp    const response = await fetch('https://santechapi.vercel.app/weather/location/chennai', {<br />
+&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp headers: {<br />
+&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 'token': '{YOUR API KEY}'<br />
+      &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp }<br />
+&nbsp &nbsp &nbsp &nbsp    });<br />
+<br />
+&nbsp &nbsp  if (!response.ok) {<br />
+&nbsp &nbsp &nbsp &nbsp  throw new Error('HTTP error! status:' + response.status);<br />
+&nbsp &nbsp  }<br />
+<br />
+&nbsp &nbsp &nbsp &nbsp const data = await response.json();<br />
+&nbsp &nbsp &nbsp &nbsp setWeatherData(data);<br />
+&nbsp &nbsp &nbsp &nbsp } catch (err) {<br />
+&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp setError(err.message);<br />
+&nbsp &nbsp &nbsp &nbsp }<br />
+&nbsp &nbsp  };<br />
+<br />
+&nbsp &nbsp      fetchWeather();<br />
+&nbsp &nbsp    }, []);<br />
 <br />
   if (error) {<br />
-    return <div>Error: {error}</div>;<br />
+&nbsp &nbsp      return <span>Error: {error}</span>;<br />
   }<br />
 
   if (!weatherData) {<br />
-    return <div>Loading weather data...</div>;<br />
+&nbsp &nbsp      return <span>Loading weather data...</span>;<br />
   }<br />
 <br />
   return (<br />
-    <div><br />
+    <span><br />
       <h1>Weather in {weatherData.location.name}</h1><br />
       <p>Temperature: {weatherData.current.temp_c}°C</p><br />
       <p>Condition: {weatherData.current.condition.text}</p><br />
       {/* Add more weather details here as needed */}<br />
-    </div><br />
+    </span><br />
   );<br />
 }<br />
 <br />
@@ -185,32 +185,33 @@ export default App;<br />`
 import { HttpClient } from '@angular/common/http';<br />
 <br />
 @Component({<br />
-  selector: 'app-weather',<br />
-  templateUrl: './weather.component.html',<br />
-  styleUrls: ['./weather.component.css']<br />
+&nbsp &nbsp    selector: 'app-weather',<br />
+&nbsp &nbsp    templateUrl: './weather.component.html',<br />
+&nbsp &nbsp    styleUrls: ['./weather.component.css']<br />
 })<br />
+<br />
 export class WeatherComponent {<br />
-  weatherData: any;<br />
-  error: string | null = null;<br />
+&nbsp &nbsp    weatherData: any;<br />
+&nbsp &nbsp  error: string | null = null;<br />
 <br />
-  constructor(private http: HttpClient) { }<br />
+&nbsp &nbsp    constructor(private http: HttpClient) { }<br />
 <br />
-  ngOnInit() {<br />
-    this.getWeatherData('chennai');<br />
-  }<br />
+&nbsp &nbsp    ngOnInit() {<br />
+&nbsp &nbsp &nbsp &nbsp  this.getWeatherData('chennai');<br />
+&nbsp &nbsp    }<br />
 <br />
-  getWeatherData(location: string) {<br />
-    this.http.get('https://santechapi.vercel.app/weather/location/' + location, {<br />
-      headers: { 'token': '{YOUR API KEY}' } // Replace with your actual API key<br />
-    }).subscribe(<br />
-      (data: any) => {<br />
-        this.weatherData = data;<br />
-      },<br />
-      (error: any) => {<br />
-        this.error = error.message;<br />
-      }<br />
-    );<br />
-  }<br />
+&nbsp &nbsp    getWeatherData(location: string) {<br />
+&nbsp &nbsp  &nbsp &nbsp      this.http.get('https://santechapi.vercel.app/weather/location/' + location, {<br />
+&nbsp &nbsp  &nbsp &nbsp        headers: { 'token': '{YOUR API KEY}' } // Replace with your actual API key<br />
+&nbsp &nbsp  &nbsp &nbsp      }).subscribe(<br />
+&nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp        (data: any) => {<br />
+&nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp          this.weatherData = data;<br />
+&nbsp &nbsp  &nbsp &nbsp        },<br />
+&nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp        (error: any) => {<br />
+&nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp          this.error = error.message;<br />
+&nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp        }<br />
+&nbsp &nbsp  &nbsp &nbsp      );<br />
+&nbsp &nbsp    }<br />
 }<br />`
   },
   {
@@ -218,7 +219,7 @@ export class WeatherComponent {<br />
     img: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png",
     code: `curl -X GET \\
   "https://santechapi.vercel.app/weather/location/chennai" \\
-  <br/> -H "token: {YOUR API KEY}"`
+  <br/>&nbsp &nbsp  -H "token: {YOUR API KEY}"`
   }
 ];
 
@@ -231,44 +232,41 @@ const axios = require('axios'); <br />
 
 const app = express(); <br />
 const port = 3000; <br />
-
+<br />
 app.get('/weather/:location', async (req, res) => {
   <br />
-  const location = req.params.location; <br />
-  const api_key = '{YOUR API KEY}'; // Replace with your actual API key<br/>
+&nbsp &nbsp const location = req.params.location; <br />
+&nbsp &nbsp  const api_key = '{YOUR API KEY}'; // Replace with your actual API key<br/>
 
-  try {
+&nbsp &nbsp  try {
     <br />
-    const response = await axios.get('https://santechapi.vercel.app/weather/location/' + location, {< br />
-      headers: { 'token': api_key } < br />
+&nbsp &nbsp    const response = await axios.get('https://santechapi.vercel.app/weather/location/' + location, { <br />
+&nbsp &nbsp      headers: { 'token': api_key } <br />
     }); <br />
 
-res.json(response.data); <br />
-  } catch (error) {
+&nbsp &nbsp res.json(response.data); <br />
+&nbsp &nbsp } catch (error) {
   <br />
-  res.status(500).json({ error: error.message }); <br />
-} <br />
+ &nbsp &nbsp res.status(500).json({ error: error.message }); <br />
+&nbsp &nbsp } <br />
 }); <br />
-
+<br/>
 app.listen(port, () => {
   <br />
-  console.log('Server listening at http://localhost:' + port); <br />
+ &nbsp &nbsp console.log('Server listening at http://localhost:' + port); <br />
 }); <br />`
   },
   {
     lang: "PHP",
     img: "https://res.cloudinary.com/hilnmyskv/image/upload/v1724919194/ui-library/icons/php.svg",
-    code: `<? php < br />
-<br />
-$api_key = '{YOUR API KEY}'; // Replace with your actual API key<br/>
+    code: `
+$api_key = '{YOUR API KEY}'; // Replace with your actual API key <br/>
 $location = 'chennai';<br/>
-<br/>
 $url = "https://santechapi.vercel.app/weather/location/".$location;<br/>
-<br/>
 $ch = curl_init($url); <br />
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); <br />
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(<br />
-    'token: '.$api_key < br />
+  &nbsp &nbsp  'token: '.$api_key <br />
 ));<br/>
 <br/>
 $response = curl_exec($ch); <br />
@@ -276,38 +274,37 @@ curl_close($ch);<br/>
 <br/>
 if ($response !== false) {
   <br />
-  $data = json_decode($response, true); <br />
-  echo "Weather in ".$data['location']['name']. ":"; <br />
-  echo "Temperature: ".$data['current']['temp_c']. "°C"; <br />
-  echo "Condition: ".$data['current']['condition']['text']; <br />
+ &nbsp &nbsp $data = json_decode($response, true); <br />
+ &nbsp &nbsp echo "Weather in ".$data['location']['name']. ":"; <br />
+ &nbsp &nbsp echo "Temperature: ".$data['current']['temp_c']. "°C"; <br />
+ &nbsp &nbsp echo "Condition: ".$data['current']['condition']['text']; <br />
 } else {
   <br />
-  echo "Error fetching weather data."; <br />
+ &nbsp &nbsp echo "Error fetching weather data."; <br />
 }<br/>
-<br/>
-  ?> <br />`
+<br/>`
   },
   {
     lang: "Python",
     img: "https://res.cloudinary.com/hilnmyskv/image/upload/v1724919194/ui-library/icons/python.svg",
     code: `import requests<br />
 
-api_key = '{YOUR API KEY}'  # Replace with your actual API key < br />
-  location = 'chennai' < br />
+api_key = '{YOUR API KEY}'  # Replace with your actual API key <br />
+  location = 'chennai' <br />
 
-    url = f'https://santechapi.vercel.app/weather/location/{location}' < br />
+    url = f'https://santechapi.vercel.app/weather/location/{location}' <br />
 
       headers = { 'token': api_key } <br />
 
-response = requests.get(url, headers = headers) < br />
+response = requests.get(url, headers = headers) <br />
 
 if response.status_code == 200: <br />
-data = response.json() < br />
-  print(f"Weather in {data['location']['name']}:") < br />
-  print(f"Temperature: {data['current']['temp_c']}°C") < br />
-  print(f"Condition: {data['current']['condition']['text']}") < br />
+&nbsp &nbsp  data = response.json() <br />
+&nbsp &nbsp  print(f"Weather in {data['location']['name']}:") <br />
+&nbsp &nbsp  print(f"Temperature: {data['current']['temp_c']}°C") <br />
+&nbsp &nbsp  print(f"Condition: {data['current']['condition']['text']}") <br />
 else: <br />
-print(f"Error: {response.text}") < br /> `
+&nbsp &nbsp print(f"Error: {response.text}") <br /> `
   },
   {
     lang: "Java",
@@ -318,59 +315,59 @@ import retrofit2.converter.gson.GsonConverterFactory; <br />
 import retrofit2.http.GET; <br />
 import retrofit2.http.Header; <br />
 import retrofit2.http.Path; <br />
-
+<br />
 public class WeatherClient {<br />
 
-  private static final String BASE_URL = "https://santechapi.vercel.app/"; <br />
-  private static final String API_KEY = "{YOUR API KEY}"; // Replace with your actual API key<br/>
-
-  private WeatherApi weatherApi; <br />
-
-  public WeatherClient() {
+&nbsp &nbsp  private static final String BASE_URL = "https://santechapi.vercel.app/"; <br />
+&nbsp &nbsp  private static final String API_KEY = "{YOUR API KEY}"; // Replace with your actual API key<br/>
+<br />
+&nbsp &nbsp  private WeatherApi weatherApi; <br />
+<br />
+&nbsp &nbsp  public WeatherClient() {
   <br />
-    Retrofit retrofit = new Retrofit.Builder() < br />
-        .baseUrl(BASE_URL) < br />
-        .addConverterFactory(GsonConverterFactory.create()) < br />
-        .build(); <br />
-  weatherApi = retrofit.create(WeatherApi.class); <br />
-} <br />
+&nbsp &nbsp &nbsp &nbsp    Retrofit retrofit = new Retrofit.Builder() <br />
+&nbsp &nbsp &nbsp &nbsp    .baseUrl(BASE_URL) <br />
+&nbsp &nbsp &nbsp &nbsp    .addConverterFactory(GsonConverterFactory.create()) <br />
+&nbsp &nbsp &nbsp &nbsp    .build(); <br />
+&nbsp &nbsp &nbsp &nbsp  weatherApi = retrofit.create(WeatherApi.class); <br />
+&nbsp &nbsp } <br />
 
-  public Call < WeatherData > getWeather(String location) {
+&nbsp &nbsp  public Call < WeatherData > getWeather(String location) {
   <br />
-  return weatherApi.getWeatherData(location); <br />
-} <br />
+ &nbsp &nbsp &nbsp &nbsp return weatherApi.getWeatherData(location); <br />
+&nbsp &nbsp } <br />
 
-public interface WeatherApi { <br/>
-@GET("weather/location/{location}") <br />
-Call < WeatherData > getWeatherData(@Path("location") String location, <br />
-                                     @Header("token") String token); <br />
-  }<br />
+&nbsp &nbsp public interface WeatherApi { <br/>
+&nbsp &nbsp &nbsp &nbsp @GET("weather/location/{location}") <br />
+&nbsp &nbsp &nbsp &nbsp Call < WeatherData > getWeatherData(@Path("location") String location, <br />
+&nbsp &nbsp &nbsp &nbsp @Header("token") String token); <br />
+&nbsp &nbsp }<br />
 
-public static class WeatherData {<br />
-    // ... (Your weather data class structure as per the API response)<br/>
-  }<br />
+&nbsp &nbsp public static class WeatherData {<br />
+&nbsp &nbsp &nbsp &nbsp // ... (Your weather data class structure as per the API response)<br/>
+&nbsp &nbsp }<br />
 }<br />
-
+<br />
 WeatherClient client = new WeatherClient(); <br />
 Call < WeatherData > call = client.getWeather("chennai"); <br />
-
-call.enqueue(new Callback < WeatherData > () {< br />
-  @Override < br />
-  public void onResponse(Call < WeatherData > call, Response < WeatherData > response) {< br />
-    if (response.isSuccessful()) {
+<br />
+call.enqueue(new Callback < WeatherData > () {<br />
+&nbsp &nbsp @Override <br />
+&nbsp &nbsp public void onResponse(Call < WeatherData > call, Response < WeatherData > response) {<br />
+&nbsp &nbsp if (response.isSuccessful()) {
   <br />
-      WeatherData weather = response.body(); <br />
-  System.out.println("Weather in " + weather.location.name + ": " + weather.current.temp_c + "°C"); <br />
-} else {
+&nbsp &nbsp &nbsp &nbsp WeatherData weather = response.body(); <br />
+&nbsp &nbsp &nbsp &nbsp System.out.println("Weather in " + weather.location.name + ": " + weather.current.temp_c + "°C"); <br />
+&nbsp &nbsp } else {
   <br />
-  System.err.println("Error: " + response.errorBody()); <br />
-} <br />
+&nbsp &nbsp &nbsp &nbsp System.err.println("Error: " + response.errorBody()); <br />
+&nbsp &nbsp} <br />
   }<br />
-
+<br />
 @Override<br />
-  public void onFailure(Call < WeatherData > call, Throwable t) {
+public void onFailure(Call < WeatherData > call, Throwable t) {
   <br />
-  System.err.println("Error: " + t.getMessage()); <br />
+&nbsp &nbsp System.err.println("Error: " + t.getMessage()); <br />
 } <br />
 }); <br />`
   },
--- src/components/home/LanguageImplementation.jsx
+++ src/components/home/LanguageImplementation.jsx
@@ -21,7 +21,7 @@ function LanguageImplementation() {
                                         <img src={currentImage} alt={lang.lang} className="h-5" />
                                         <h1 className="text-xl md:text-2xl font-bold">{lang.lang}</h1>
                                     </div>
-                                    <span className="hidden sm:flex px-3 py-2 bg-violet-100 text-violet-700 rounded-lg">
+                                    <span className="hidden font-semibold sm:flex px-3 py-2 bg-violet-100 text-violet-800 rounded-lg">
                                         Weather API
                                     </span>
                                 </div>
@@ -62,7 +62,7 @@ function LanguageImplementation() {
                                     <button
                                         key={item.lang}
                                         className={`px-5 py-3 break-words font-semibold border rounded-lg transition duration-300 ease-in-out shadow-md focus:outline-none focus:ring-2 focus:ring-violet-500 ${lang.lang === item.lang
-                                            ? 'bg-violet-600 text-white border-violet-500'
+                                            ? 'bg-violet-700 text-white border-violet-500'
                                             : 'bg-white text-violet-600 hover:bg-violet-100'
                                             }`}
                                         onClick={() => setLang(item)}
