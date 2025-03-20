bfc2321573560f87f4490eb5d59af38e77ae6e78
Santhosh20112003 <88790224+Santhosh20112003@users.noreply.github.com>
2025-03-20 05:09:02 +0000

    update UI components for better user experience and accessibility

---
+++ sample.js
@@ -0,0 +1,178 @@
+[
+    {
+      "name": "Weather API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Real-time weather data, forecasts, and conditions",
+      "description": "API for real-time weather data, forecasts, and conditions"
+    },
+    {
+      "name": "Ecommerce API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Sample ECommerce products and their details",
+      "description": "API for Sample ECommerce products and their Details"
+    },
+    {
+      "name": "Quotes API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Inspirational and notable sayings",
+      "description": "API for famous quotes: inspirational and notable sayings"
+    },
+    {
+      "name": "Jokes API",
+      "apiurl": "try",
+      "docs_url": "https://santech.gitbook.io/docs/weather-api",
+      "capabilities": "Humorous content and punchlines",
+      "description": "API for fetching jokes: humorous content and punchlines"
+    },
+    {
+      "name": "News API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Latest news articles and headlines",
+      "description": "API for fetching the latest news articles and headlines"
+    },
+    {
+      "name": "Sports Player API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Sports player details, stats, and profiles",
+      "description": "API for fetching sports player details: stats and profiles"
+    },
+    {
+      "name": "Stocks API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Stock market data for real-time financial insights",
+      "description": "API for stock market data used in real-time financial insights"
+    },
+    {
+      "name": "Random User API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Random fake user info: gender, name, email, address",
+      "description": "API for random fake user info: gender, name, email, address"
+    },
+    {
+      "name": "University API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Fetch a list of universities in a specified country",
+      "description": "API to get a list of universities in a specified country"
+    },
+    {
+      "name": "Music API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Retrieve music details: artists, albums, and tracks",
+      "description": "API for retrieving music details: artists, albums, and tracks"
+    },
+    {
+      "name": "Dictionary API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Fetch English word meaning, definitions, and audio",
+      "description": "API for fetching English word meaning, definitions, audio, etc."
+    },
+    {
+      "name": "Currency API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Convert currency to different country rates",
+      "description": "API for converting currency to different country rates."
+    },
+    {
+      "name": "Genderize API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Predict gender based on name",
+      "description": "API for Predicting the gender of a person based on their name."
+    },
+    {
+      "name": "QrCode Generator API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Generate QR codes with data and custom size",
+      "description": "API to Generate QR code with data and custom size."
+    },
+    {
+      "name": "Online Games API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Fetch free online browser games: titles and genres",
+      "description": "API to fetch free online browser games: titles and genres"
+    },
+    {
+      "name": "Anime Details API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Fetch anime movies and series details: titles, genres, episodes",
+      "description": "API for fetching anime movies and series details: titles, genres, episodes"
+    },
+    {
+      "name": "Books API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Fetch book details: titles, authors, and summaries",
+      "description": "API to fetch book details: titles, authors, and summaries"
+    },
+    {
+      "name": "Maps API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Fetch location details: coordinates and info",
+      "description": "API for fetching location details: coordinates and info"
+    },
+    {
+      "name": "Images API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Fetch images based on search terms and queries",
+      "description": "API to fetch images based on search terms and queries"
+    },
+    {
+      "name": "Recipe API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Fetch recipes and calories in a food item",
+      "description": "API for fetching recipes and calories in a food item."
+    },
+    {
+      "name": "Giphy & Stickers API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Fetch GIFs and stickers data: GIFs and reactions",
+      "description": "API to fetch Giphy and stickers data: GIFs and reactions"
+    },
+    {
+      "name": "Movie & Series API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Fetch movie and series details: titles and ratings",
+      "description": "API to fetch movies and series details: titles and ratings"
+    },
+    {
+      "name": "Conversational API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Conversational chatbot: dialogue and responses",
+      "description": "API for a conversational chatbot: dialogue and responses"
+    },
+    {
+      "name": "Insightful Blog API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Access blogs from Insightful Blog Contents",
+      "description": "API for accessing blogs from Insightful Blog Contents"
+    },
+    {
+      "name": "Jarvis AI API",
+      "apiurl": "try",
+      "docs_url": "N/A",
+      "capabilities": "Generative AI assistant for tasks and queries",
+      "description": "API for Gemini Flash generative AI assistant: tasks and queries"
+    }
+  ]
+  
