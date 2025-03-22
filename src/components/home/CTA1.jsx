e018fc879b1348721308a63a892c2ccf7327c3d1
Santhosh20112003 <88790224+Santhosh20112003@users.noreply.github.com>
2025-03-22 10:49:14 +0000

    update image source in CTA1 component for improved visual representation

--- src/components/home/CTA1.jsx
+++ src/components/home/CTA1.jsx
@@ -9,7 +9,7 @@ function CTA1() {
     <section className="text-gray-600 body-font">
       <div className="w-full mx-auto flex lg:px-16 xl:px-20 py-24 md:flex-row flex-col items-center">
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          <img src="https://ik.imagekit.io/ikmedia/New_website_graphics/DAM_Storage/In-built_media_optimizations_and_transformations-01_m2FOU_H3v.png" className="object-cover object-center rounded" />
+          <img src="https://ik.imagekit.io/vituepzjm/APIHUB/Macbook-Air-santechapihubs.vercel.app%20(1).png?updatedAt=1742640396262" className="object-cover object-center rounded" />
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-32 px-5 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="px-8 py-3 bg-gray-100 uppercase text-sm rounded-full mb-5 font-bold text-gray-600">AI-Powered Innovation</h1>
